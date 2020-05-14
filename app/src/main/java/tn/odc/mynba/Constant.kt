795e3203c7a936e073a098345d90ed97047b3247
Oussama Aroua <oussama.aroua@orange.com>
2020-05-14 10:28:53 +0100

    Initial commit

---
+++ app/src/main/java/tn/odc/mynba/Constant.kt
@@ -0,0 +1,4 @@
+package tn.odc.mynba
+
+const val BASE_URL = "http://www.mocky.io"
+const val END_POINT = "/v2/5de8d38a3100000f006b1479"
---
+++ app/src/main/java/tn/odc/mynba/MainActivity.kt
@@ -0,0 +1,15 @@
+package tn.odc.mynba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.ListAdapter
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
