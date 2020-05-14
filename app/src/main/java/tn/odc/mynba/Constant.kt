6bcabdbbd21345acf9ee4d80e697dedcdb647c1d
Oussama Aroua <oussama.aroua@orange.com>
2020-05-14 11:11:04 +0100

    Update Links

--- app/src/main/java/tn/odc/mynba/Constant.kt
+++ app/src/main/java/tn/odc/mynba/Constant.kt
@@ -1,4 +1,7 @@
 package tn.odc.mynba
 
-const val BASE_URL = "http://www.mocky.io"
-const val END_POINT = "/v2/5de8d38a3100000f006b1479"
+//const val BASE_URL = "http://www.mocky.io"
+//const val END_POINT = "/v2/5de8d38a3100000f006b1479"
+
+const val USER_BASE_URL = "https://evenement.azurewebsites.net/"
+const val USER_END_POINT = "api/Users1"
